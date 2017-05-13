de6e8ce0655535d75b2061f0be284cc10367563d
Craig McDaniel <craig.mcdaniel@datastax.com>
2017-05-13 10:09:50 -0400

    Hoplon files should use clojurescript-mode

--- .oh-my-zsh/custom/automaton.zsh
+++ .oh-my-zsh/custom/automaton.zsh
@@ -1,2 +1,2 @@
-export PYTHONPATH=${HOME}/automaton:${PYTHONPATH}
-export PATH=${HOME}/automaton/bin:${PATH}
+#export PYTHONPATH=${HOME}/automaton:${PYTHONPATH}
+#export PATH=${HOME}/automaton/bin:${PATH}
