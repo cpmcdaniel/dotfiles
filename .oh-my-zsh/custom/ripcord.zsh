4af23acf213a95f7391768e506a29c945558aaff
Craig McDaniel <craig.mcdaniel@datastax.com>
2019-04-18 10:45:19 -0400

    Adding ripcord aliases

--- .oh-my-zsh/custom/ripcord.zsh
+++ .oh-my-zsh/custom/ripcord.zsh
@@ -1,3 +1,7 @@
 if [[ -d ${HOME}/ripcord ]] then
    export PATH=${HOME}/ripcord/opscenterd/node_modules/.bin:${PATH}
 fi
+
+alias .gr="cd ${HOME}/ripcord; ./gradlew --no-daemon --rerun-tasks --parallel"
+alias .grx="cd ${HOME}/ripcord; ./gradlew"
+alias .opsc="cd ${HOME}/ripcord/opscenterd; ./bin/opscenter -f"
