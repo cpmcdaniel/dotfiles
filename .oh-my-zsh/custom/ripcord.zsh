d6dc20e0bd540f74d4abf93be16bedda2461412c
Craig McDaniel <cpmcdaniel@gmail.com>
2020-01-10 10:37:33 -0500

    More gradle aliases.

--- .oh-my-zsh/custom/ripcord.zsh
+++ .oh-my-zsh/custom/ripcord.zsh
@@ -2,8 +2,16 @@ if [[ -d ${HOME}/ripcord ]] then
    export PATH=${HOME}/ripcord/opscenterd/node_modules/.bin:${PATH}
 fi
 
-alias .gr="cd ${HOME}/ripcord; ./gradlew --no-daemon --rerun-tasks --parallel"
-alias .grx="cd ${HOME}/ripcord; ./gradlew"
+alias .gr="cd ${HOME}/ripcord; ./gradlew"
+alias .grp=".gr --parallel"
+alias .grr=".gr --no-daemon --rerun-tasks"
+alias .grx=".grr --parallel"
+alias .grk=".gr --stop"
+alias .grc=".gr clean"
 alias .opsc="cd ${HOME}/ripcord/opscenterd; ./bin/opscenter -f"
 alias .merge="cd ${HOME}/ripcord; ./bin/merge.py"
 alias .afts="cd ${HOME}/ripcord/spock/afts/q ; source ~/.venv/lcm-afts/bin/activate"
+alias .tacb="cd ${HOME}/ripcord; .gr \
+      :configbuilder:test :spock:test \
+      :configbuilder-cli:shadowJar :configbuilder-cli:test \
+      :dse-server-config-builder:test"
