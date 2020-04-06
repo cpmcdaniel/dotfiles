169ef6a94b397b07e546ecd3cb29b88ce6f624dc
Craig McDaniel <cpmcdaniel@gmail.com>
2020-04-06 15:27:52 -0400

    Updating dotfiles

---
+++ .oh-my-zsh/custom/bdp.zsh
@@ -0,0 +1,9 @@
+export JAVA8_HOME=$HOME/.sdkman/candidates/java/8.0.232-zulu
+export JAVA11_HOME=$HOME/.sdkman/candidates/java/11.0.6-open
+
+
+# CCM STUFF
+export DEBUG=true
+export PRINT_DEBUG=true
+export KEEP_TEST_DIR=true
+export APOLLO_DIR=/home/craig/bdp
